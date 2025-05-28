6dc619cacaa044cbd29ec870ccc7f7e3f11a11c2
Bak Hyeongwoo <toplionby421@gmail.com>
2025-05-28 11:32:46 +0900

    feat : 푸시알림 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
-import 'firstpage.dart'; // firstpage.dart import
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'pushalarm_message.dart';
+import 'firstpage.dart'; // 시작화면
+import 'notification.dart';
 
-void main() {
+final navigatorKey = GlobalKey<NavigatorState>();
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 로컬 푸시 알림 초기화
+  await initializeNotification();
+
+  // 앱이 종료된 상태에서 알림 클릭 시 처리
+  final NotificationAppLaunchDetails? notificationAppLaunchDetails =
+      await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+
+  if (notificationAppLaunchDetails?.didNotificationLaunchApp ?? false) {
+    Future.delayed(const Duration(seconds: 1), () {
+      navigatorKey.currentState!.pushNamed(
+        '/message',
+        arguments: notificationAppLaunchDetails?.notificationResponse?.payload,
+      );
+    });
+  }
+
   runApp(const MyApp());
 }
 
@@ -11,8 +36,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       title: 'Navigator Demo',
-      home: const FirstPage(), // 첫 화면을 FirstPage로 지정
+      home: const FirstPage(), // 시작 화면 유지
+      routes: {
+        '/message': (context) => const MessagePage(),
+      },
     );
   }
 }
